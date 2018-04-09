9c7d1b548ef4e23d0ca0512b698039a19b961897
MAIN\jrp8297 <jrp8297@gmail.com>
2018-04-09 15:45:36 -0400

    Fixed Hover Varience

--- DX11Starter/Creature.cpp
+++ DX11Starter/Creature.cpp
@@ -114,7 +114,7 @@ void Creature::Update(float deltaTime, float totalTime)
 		isFeedingDuration += deltaTime;
 
 		// reset appropriate variables if we are done feeding
-		if (isFeedingDuration > 3.0f) {
+		if (isFeedingDuration > 6.28f) {
 			isFeeding = false;
 			isFeedingDuration = 0;
 		}
